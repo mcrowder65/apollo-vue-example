d8754e00af138e26b81aae9d448a4d4b68fc25fb
Matt Crowder <mcrowder65@gmail.com>
2019-04-01 13:05:12 -0400

    GET_POSTS is calling network

---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ GET_POSTS }}
+  </div>
+</template>
+
+<script>
+import { GET_POSTS } from "../graphql/queries";
+
+export default {
+  name: "Posts",
+  apollo: {
+    GET_POSTS
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,35 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
+import VueApollo from "vue-apollo";
+
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// HTTP connexion to the API
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000/graphql"
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+Vue.use(VueApollo);
 
 Vue.use(VueMaterial);
 Vue.config.productionTip = false;
-
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
 new Vue({
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
